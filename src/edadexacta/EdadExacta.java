fa63bb10c1f4d28282fd225e388dc3bf9312cae1
starFaby <star._faby@hotmail.com>
2019-08-29 22:52:58 -0500

    calcular edad años meses y diaz exactos

---
+++ src/edadexacta/EdadExacta.java
@@ -0,0 +1,22 @@
+package edadexacta;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class EdadExacta {
+
+    public static void main(String[] args) throws ParseException {
+//       Date date = new Date();
+//       int d = date.getDay();
+//        System.out.println("==> "+d);
+//        Calendar ca = Calendar.getInstance();
+//        int ano = ca.get(Calendar.DAY_OF_MONTH);
+//        System.out.println("ano "+ano);
+
+       
+    }
+
+}
